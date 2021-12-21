8bbba3cb555a15266f88918750873e8c10836c1f
keith72632 <keithhorton30@outlook.com>
2021-12-20 18:09:26 -0600

    SCB

---
+++ SystemControlBlock/Debug/Inc/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Inc/faults.cpp \
+../Inc/scb.cpp 
+
+OBJS += \
+./Inc/faults.o \
+./Inc/scb.o 
+
+CPP_DEPS += \
+./Inc/faults.d \
+./Inc/scb.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Inc/%.o: ../Inc/%.cpp Inc/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m4 -std=gnu++14 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Inc
+
+clean-Inc:
+	-$(RM) ./Inc/faults.d ./Inc/faults.o ./Inc/scb.d ./Inc/scb.o
+
+.PHONY: clean-Inc
+
---
+++ SystemControlBlock/Inc/faults.h
@@ -0,0 +1,19 @@
+/*
+ * faults.h
+ *
+ *  Created on: Dec 20, 2021
+ *      Author: khorton
+ */
+
+#ifndef FAULTS_H_
+#define FAULTS_H_
+
+#include <stdio.h>
+
+namespace Faults {
+	void gen_usage_fault();
+	void gen_memory_fault();
+}
+
+
+#endif /* FAULTS_H_ */
