be126ef12a12db77f75f9995884b7ee30480015d
keith72632 <keithhorton30@outlook.com>
2021-12-25 18:08:11 -0600

    usart string working

---
+++ Uart/Debug/Inc/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../Inc/usart.cpp 
+
+OBJS += \
+./Inc/usart.o 
+
+CPP_DEPS += \
+./Inc/usart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Inc/%.o: ../Inc/%.cpp Inc/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m4 -std=gnu++14 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Inc
+
+clean-Inc:
+	-$(RM) ./Inc/usart.d ./Inc/usart.o
+
+.PHONY: clean-Inc
+
--- Uart/Debug/Src/subdir.mk
+++ Uart/Debug/Src/subdir.mk
@@ -5,29 +5,38 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/main.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
+CPP_SRCS += \
+../Src/main.cpp \
+../Src/usart.cpp 
+
 C_DEPS += \
-./Src/main.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
 OBJS += \
 ./Src/main.o \
 ./Src/syscalls.o \
-./Src/sysmem.o 
+./Src/sysmem.o \
+./Src/usart.o 
+
+CPP_DEPS += \
+./Src/main.d \
+./Src/usart.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+Src/%.o: ../Src/%.cpp Src/subdir.mk
+	arm-none-eabi-g++ "$<" -mcpu=cortex-m4 -std=gnu++14 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -O0 -ffunction-sections -fdata-sections -fno-exceptions -fno-rtti -fno-use-cxa-atexit -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Src/%.o: ../Src/%.c Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/main.d ./Src/main.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o
+	-$(RM) ./Src/main.d ./Src/main.o ./Src/syscalls.d ./Src/syscalls.o ./Src/sysmem.d ./Src/sysmem.o ./Src/usart.d ./Src/usart.o
 
 .PHONY: clean-Src
 
